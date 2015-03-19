101234abe94c31610b400b4d0c7ca299175c2c16
mikehajj <mike.hajj@gmail.com>
2015-03-19 21:06:59 +0200

    added shelljs, updated tests

--- test/integration/_servers.test.js
+++ test/integration/_servers.test.js
@@ -1,3 +1,23 @@
 var controller = require("soajs.controller");
 var oauth = require("soajs.oauth");
 var urac = require("soajs.urac");
+
+var shell = require('shelljs');
+describe("importing sample data", function() {
+	it("do import", function(done) {
+
+		shell.pushd(__dirname + '/../../tools/');
+		shell.exec('./soajs.mongo.sh', function(code, output) {
+			console.log('test data imported.')
+			shell.popd();
+			done();
+		});
+
+	});
+
+	after(function(done) {
+		setTimeout(function() {
+			done();
+		}, 1000);
+	});
+});
