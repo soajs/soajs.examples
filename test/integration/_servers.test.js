b5cf5e0e592f3b51abec79119ed9dd15dcecd1d8
mikehajj <mike.hajj@gmail.com>
2015-03-24 15:17:50 +0200

    removed tools from module, added soajs.mongodb.data as devDependency, updated grunt configuration and tests to use soajs.mongodb.data, bumped version

--- test/integration/_servers.test.js
+++ test/integration/_servers.test.js
@@ -1,24 +1,28 @@
 "use strict";
 var controller, urac, oauth;
+var assert = require('assert');
 var shell = require('shelljs');
+var sampleData = require("soajs.mongodb.data/modules/examples");
 
 describe("importing sample data", function() {
 	it("do import", function(done) {
-
-		shell.pushd(__dirname + '/../../tools/');
-		shell.exec('./soajs.mongo.sh', function(code, output) {
-			console.log('test data imported.');
-			shell.popd();
-			controller = require("soajs.controller");
-			oauth = require("soajs.oauth");
-			urac = require("soajs.urac");
-			done();
+		shell.pushd(sampleData.dir);
+		shell.exec("chmod +x " + sampleData.shell, function(code) {
+			assert.equal(code, 0);
+			shell.exec(sampleData.shell, function(code) {
+				assert.equal(code, 0);
+				shell.popd();
+				done();
+			});
 		});
-
 	});
 
 	after(function(done) {
+		console.log('test data imported.');
 		setTimeout(function() {
+			controller = require("soajs.controller");
+			oauth = require("soajs.oauth");
+			urac = require("soajs.urac");
 			done();
 		}, 1000);
 	});
